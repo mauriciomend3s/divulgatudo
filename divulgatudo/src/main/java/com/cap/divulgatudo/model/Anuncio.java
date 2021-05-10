8b86bec0271eb2e614b575e87a66172b12ebbbf0
Maurício Mendes <mauricio.mend3s@gmail.com>
2021-05-10 12:45:49 -0300

    model anuncio

---
+++ divulgatudo/src/main/java/com/cap/divulgatudo/model/Anuncio.java
@@ -0,0 +1,70 @@
+package com.cap.divulgatudo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Anuncio {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String id_Anuncio;
+	private String nome_Anuncio;
+	private String cliente;
+	private String data_Inicio;
+	private String data_Termino;
+	private double investimento_Dia;
+
+	public Anuncio() {
+
+	}
+
+	public String getId_Anuncio() {
+		return id_Anuncio;
+	}
+
+	public void setId_Anuncio(String id_Anuncio) {
+		this.id_Anuncio = id_Anuncio;
+	}
+
+	public String getNome_Anuncio() {
+		return nome_Anuncio;
+	}
+
+	public void setNome_Anuncio(String nome_Anuncio) {
+		this.nome_Anuncio = nome_Anuncio;
+	}
+
+	public String getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+
+	public String getData_Inicio() {
+		return data_Inicio;
+	}
+
+	public void setData_Inicio(String data_Inicio) {
+		this.data_Inicio = data_Inicio;
+	}
+
+	public String getData_Termino() {
+		return data_Termino;
+	}
+
+	public void setData_Termino(String data_Termino) {
+		this.data_Termino = data_Termino;
+	}
+
+	public double getInvestimento_Dia() {
+		return investimento_Dia;
+	}
+
+	public void setInvestimento_Dia(double investimento_Dia) {
+		this.investimento_Dia = investimento_Dia;
+	}
+}

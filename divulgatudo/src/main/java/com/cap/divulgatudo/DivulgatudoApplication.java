eba02d5136db1a451f7816b72d20f1afd773def2
Maurício Mendes <mauricio.mend3s@gmail.com>
2021-05-10 11:03:49 -0300

    primeiro commit

---
+++ divulgatudo/src/main/java/com/cap/divulgatudo/DivulgatudoApplication.java
@@ -0,0 +1,13 @@
+package com.cap.divulgatudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivulgatudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivulgatudoApplication.class, args);
+	}
+
+}
---
+++ divulgatudo/src/test/java/com/cap/divulgatudo/DivulgatudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cap.divulgatudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivulgatudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
